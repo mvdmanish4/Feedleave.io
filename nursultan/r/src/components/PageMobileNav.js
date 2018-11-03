4f4ff9e2808b622e797cc56e783bb89483323c07
Alton Fong <altonf@vmware.com>
2018-11-03 18:04:58 +0100

    Add auth logic to mobile nav bar
    []

--- nursultan/r/src/components/PageMobileNav.js
+++ nursultan/r/src/components/PageMobileNav.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
 import { Menu, Icon } from 'semantic-ui-react'
 import PropTypes from 'prop-types'
+import firebase from 'firebase/app'
 import { NavLink } from 'react-router-dom'
 
 const propTypes = {
   subpages: PropTypes.object.isRequired
 }
 class PageMobileNav extends Component {
+  componentWillMount() {
+    this.authListener = this.authListener.bind(this)
+    this.authListener()
+    this.setState({ loggedIn: false })
+  }
+  componentWillUnmount() {
+    this.fireBaseListener && this.fireBaseListener()
+    this.authListener = undefined
+  }
   renderMenuItems = subpages => {
     const menuItems = Object.keys(subpages).map(page => {
       return (
@@ -29,6 +39,27 @@ class PageMobileNav extends Component {
   render() {
     const { subpages } = this.props
     const menuItems = this.renderMenuItems(subpages)
+    if (this.state.loggedIn) {
+      return (
+        <div>
+          <Menu.Item link key={'home'} name={'home'} as={NavLink} exact to={'/'}>
+            <Icon name={'home'} src={'/'} /> Home
+          </Menu.Item>
+          <Menu.Item link key={'reviews'} name={'reviews'} as={NavLink} exact to={'/reviews'}>
+            <Icon name={'book'} src={'/reviews'} /> Reviews
+          </Menu.Item>
+          <Menu.Item link key={'write'} name={'write'} as={NavLink} exact to={'/write'}>
+            <Icon name={'write'} src={'/write'} /> Write
+          </Menu.Item>
+          <Menu.Item link key={'profile'} name={'profile'} as={NavLink} exact to={'/profile'}>
+            <Icon name={'settings'} src={'/profile'} /> My Profile
+          </Menu.Item>
+          <Menu.Item link key={'signout'} name={'signout'} as={NavLink} exact to={'/signout'}>
+            <Icon name={'sign out'} src={'/signout'} /> Sign Out
+          </Menu.Item>
+        </div>
+      )
+    }
     return (
       <div>
         <Menu.Item link key={'home'} name={'home'} as={NavLink} exact to={'/'}>
@@ -54,6 +85,30 @@ class PageMobileNav extends Component {
         </Menu.Item>
       </div>
     )
+  }
+  authListener() {
+    this.fireBaseListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        // User is signed in.
+        // console.log('logged in')
+        this.setState({
+          loggedIn: true,
+          displayName: user.displayName,
+          email: user.email,
+          emailVerified: user.emailVerified,
+          photoURL: user.photoURL,
+          isAnonymous: user.isAnonymous,
+          uid: user.uid,
+          providerData: user.providerData
+        })
+        // ...
+      } else {
+        // User is signed out.
+        // ...
+        this.setState({ loggedIn: false })
+        // console.log('logged out')
+      }
+    })
   }
 }
 
